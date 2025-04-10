563046ccf34fd5b82593bc82ffb17f3385d11545
Cat-In-Case <52603485+Cat-In-Case@users.noreply.github.com>
2025-04-10 17:37:59 +0900

    ParallelTask Fixed

--- Test/ParallelTaskBenchmark.cs
+++ Test/ParallelTaskBenchmark.cs
@@ -14,18 +14,31 @@ using System.Diagnostics;
 using System.Threading.Channels;
 using Debug = UnityEngine.Debug;
 using UnityEditor.ShaderGraph.Internal;
+using Unity.Profiling;
 
 public class ParallelTaskBenchmark : MonoBehaviour
 {
     [System.Runtime.InteropServices.DllImport("kernel32.dll")]
     public static extern uint GetCurrentProcessorNumber();
 
-    public Vector3[] results_parallaxTask = new Vector3[240];
-    public Vector3[] results_job = new Vector3[240];
+    public Vector3[] results_parallaxTask = new Vector3[240];       
+    public Vector3[] results_job = new Vector3[240];        //초기
 
     const int TotalCount = 240;
     const int TaskCount = 4;
     const int ChunkSize = TotalCount / TaskCount;
+
+    public struct VecWithIndex
+    {
+        public int index;
+        public Vector3 vec;
+
+        public VecWithIndex(int i, Vector3 v)
+        {
+            index = i;
+            vec = v;
+        }
+    }
 
     void Start()
     {
@@ -33,39 +46,77 @@ public class ParallelTaskBenchmark : MonoBehaviour
         //StartCoroutine(RunJobCoroutine());
     }
 
-    public float A(int i)
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad9))
+        {
+            StartCoroutine(RunParallelTaskBenchmark());
+        }
+    }
+
+    public static float A(int i)
     {
         var random = new System.Random(i);
         return (float)(random.NextDouble());
     }
+    private static void ParallelCal(int taskIndex, int ChunkSize, Channel<VecWithIndex> channel)
+    {
+        // Parallel.For는 현재 쓰레드 풀 내의 가용 CPU 코어를 분할하여 병렬로 처리합니다.
+        Parallel.For(0, ChunkSize, i =>
+        {
+            int globalIndex = taskIndex * ChunkSize + i;
+            float x = A(globalIndex) * 22f - 11f;
+            float y = A(globalIndex + 1) * 22f - 11f;
+            float z = A(globalIndex + 2) * 22f - 11f;
 
+            channel.Writer.TryWrite(new VecWithIndex(globalIndex, new Vector3(x, y, z))); // Channel에 값 쓰기
+
+            // 각 쓰레드의 Managed ID, 논리 CPU (OS 스케줄 기준) 로그 출력
+            //var currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            //int processorId = -1;
+            try
+            {
+                //processorId = (int)GetCurrentProcessorNumber();
+            }
+            catch { }
+
+            //Debug.Log($"[Thread ID: {currentThreadId}] [Vec Index: {globalIndex}] [CPU ID: {processorId}]");
+        });
+    }
     IEnumerator RunParallelTaskBenchmark()
     {
-        Profiler.BeginSample("ParallaxTaskBenchmark");
+        //Profiler.BeginSample("ParallaxTaskBenchmark");
+        var gcRecorder = ProfilerRecorder.StartNew(ProfilerCategory.Memory, "GC Allocated In Frame");
+
         Debug.Log("▶ Parallax + Task 시작");
         Stopwatch sw = Stopwatch.StartNew();
 
         // Channel은 병렬 작업(Task)이 생산한 데이터를 비동기적으로 소비자(Coroutine)에게 전달하기 위해 사용됨
-        // ThreadSafe임
-        // Reader/Writer 모델, 비동기 스트리밍 최적화
-        // CreateUnbounded는 크기 제한 없음
-        Channel<(int index, Vector3 vec)> channel = Channel.CreateUnbounded<(int, Vector3)>();
+        // ThreadSafe임 + Reader/Writer 모델, 비동기(await) 스트리밍 최적화
+        // CreateUnbounded는 크기 제한 없음 => 가비지 발생량 높음 ----> CreateBounded(Capacity)사용 권장
+        // NativeArray이나 class + ArrayPool<class>의 형태를 사용 권장 => 가비지 프리
+        //ArrayPool<VecWithIndex> (class)	NativeArray<VecWithIndex> (struct)
+        Channel<VecWithIndex> channel = Channel.CreateBounded<VecWithIndex>(240);
         Task[] taskList = new Task[TaskCount];
+
+        TaskScheduler scheduler = TaskScheduler.FromCurrentSynchronizationContext();        //컨텍스트를 메인스레드와 동기화
 
         for (int t = 0; t < TaskCount; t++)
         {
             int taskIndex = t;//쓰레드 병렬
-            taskList[t] = Task.Run(() =>
+            Profiler.BeginSample("Delegate");
+
+            taskList[t] = Task.Factory.StartNew(() =>
             {
                 // Parallel.For는 현재 쓰레드 풀 내의 가용 CPU 코어를 분할하여 병렬로 처리합니다.
                 Parallel.For(0, ChunkSize, i =>
                 {
                     int globalIndex = taskIndex * ChunkSize + i;
-                    float x = A(globalIndex) * 22f - 11f;
-                    float y = A(globalIndex + 1) * 22f - 11f;
-                    float z = A(globalIndex + 2) * 22f - 11f;
+                    //float x = A(globalIndex) * 22f - 11f;
+                    //float y = A(globalIndex + 1) * 22f - 11f;
+                    //float z = A(globalIndex + 2) * 22f - 11f;
 
-                    channel.Writer.TryWrite((globalIndex, new Vector3(x, y, z))); // Channel에 값 쓰기
+                    //channel.Writer.TryWrite(new VecWithIndex(globalIndex, new Vector3(x, y, z))); // Channel에 값 쓰기
 
                     // 각 쓰레드의 Managed ID, 논리 CPU (OS 스케줄 기준) 로그 출력
                     var currentThreadId = Thread.CurrentThread.ManagedThreadId;
@@ -78,7 +129,8 @@ public class ParallelTaskBenchmark : MonoBehaviour
 
                     Debug.Log($"[Thread ID: {currentThreadId}] [Vec Index: {globalIndex}] [CPU ID: {processorId}]");
                 });
-            });
+            }, CancellationToken.None, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);//scheduler);
+            Profiler.EndSample();
         }
 
         // ArrayPool: GC 힙 할당 없이 메모리 재사용을 위한 풀 (CLR에서 관리)
@@ -87,32 +139,52 @@ public class ParallelTaskBenchmark : MonoBehaviour
         int insertIndex = 0;
 
         // Reader 처리를 별도 Task로 실행하여 Coroutine에서 await 피함
+        /*
         Task readerTask = Task.Run(async () =>
         {
+            int i = 0;
             while (await channel.Reader.WaitToReadAsync())      //채널에 읽을 값이 들어올 때까지 비동기 대기, Complete가 호출되면 false반환
             {
                 while (channel.Reader.TryRead(out var pair))        //내부가 비어있으면 false
                 {
-                    int index = pair.index;
-                    if (index < TotalCount)
-                        pooledArray[index] = pair.vec;
+                    i = pair.index;
+                    if (i < TotalCount)
+                        pooledArray[i] = pair.vec;
                 }
             }
         });
-        yield return new WaitUntil(() => taskList.All(task => task.IsCompleted));        // 모든 Task가 완료될 때까지 대기
+        */
+        yield return new WaitUntil(() => Task.WhenAll(taskList).IsCompleted);        // 모든 Task가 완료될 때까지 대기
         channel.Writer.Complete();                      // 채널 작성을 종료
-        yield return new WaitUntil(() => readerTask.IsCompleted);                       // channel의 read 종료
-
+        //yield return new WaitUntil(() => readerTask.IsCompleted);                       // channel의 read 종료
+       
         Array.Copy(pooledArray, results_parallaxTask, TotalCount);
         ArrayPool<Vector3>.Shared.Return(pooledArray); // 풀에 반환하여 재사용 가능하게 함
 
         sw.Stop();
-        Debug.Log($"? Parallax + Task 완료: {sw.ElapsedMilliseconds} ms");
-        Profiler.EndSample();
+        //Debug.Log($"? Parallax + Task 완료: {sw.ElapsedMilliseconds} ms");
+        //Profiler.EndSample();
 
+        foreach (var task in taskList)
+        {
+            task.Dispose(); // Task 해제
+        }
+        //readerTask.Dispose(); // Reader Task 해제
+        sw = null;
+        channel = null;
+        /*
         long memory = GC.GetTotalMemory(false);
         Debug.Log($"?? GC Memory 사용량: {memory / 1024f:N2} KB");
+        // GC 메모리 출력 (프레임 내 할당량)
+        if (gcRecorder.Valid)
+        {
+            long gcBytes = gcRecorder.LastValue;
+            Debug.Log($"ProfilerRecorder - GC Allocated In Frame: {gcBytes / 1024f:N2} KB");
+        }
+        gcRecorder.Dispose();
+
         Debug.Log("?? 코루틴 종료");
+        */
     }
 
     IEnumerator RunJobCoroutine()
